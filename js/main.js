7bff02b8c50e6ce055e43e13220661400456de3e
danklingjr <danielklingjr@gmail.com>
2014-09-29 16:33:42 -0400

    service demo

--- js/main.js
+++ js/main.js
@@ -39,3 +39,83 @@ if(jQuery(window).scrollTop() > 1000){
     });
   });
 };
+jQuery(".whoa").on("click", function(){
+
+    //Desktop////////////////////////////////////////////
+    jQuery(".browser-outer").animate({
+      width: "100%"//,
+      //height: "537px"
+    }, 600);
+    jQuery(".block-1").animate({
+      width: "25%"
+    }, 600);
+    jQuery(".browser-inner").animate({
+      marginTop: "0"
+    }, 600);
+    jQuery(".browser-bottom").animate({
+      opacity: "0"
+    }, 600);
+    jQuery(".first-circle").animate({
+      left: "0"
+    }, 600);
+    jQuery(".second-circle").animate({
+      left: "0",
+      width: "10px",
+      marginLeft: "0"
+    }, 600);
+    jQuery(".last-circle").animate({
+      opacity: "1"
+    }, 600);
+    
+    //Tablet////////////////////////////////////////////
+    jQuery(".browser-outer").delay(2400).animate({
+      width: "40%",
+      height: "682px"
+    }, 600);
+    jQuery(".browser-bottom").delay(2600).animate({
+      opacity: "1"
+    }, 600);
+    jQuery(".block-1").delay(2400).animate({
+      width: "50%"
+    }, 600);
+    
+    //Device attributes
+    jQuery(".first-circle").delay(2400).animate({
+      left: "38%"
+    }, 600);
+    jQuery(".second-circle").delay(2400).animate({
+      left: "48%",
+      width: "50px",
+      marginLeft: "-25px"
+    }, 600);
+    jQuery(".last-circle").delay(2400).animate({
+      opacity: "0"
+    }, 600);
+    
+    //Content scrolling
+    jQuery(".browser-inner").delay(3000).animate({
+      marginTop: "-180px"
+    }, 1500);
+    jQuery(".browser-inner").delay(3500).animate({
+      marginTop: "0px"
+    }, 600);
+    
+    
+    
+    //Mobile////////////////////////////////////////////
+    jQuery(".browser-outer").delay(4800).animate({
+      width: "20%",
+      height: "600px"
+    }, 600);
+    jQuery(".block-1").delay(4800).animate({
+      width: "100%"
+    }, 600);
+    
+    //Content scrolling
+    jQuery(".browser-inner").delay(2000).animate({
+      marginTop: "-900px"
+    }, 1500);
+    
+
+  
+});
