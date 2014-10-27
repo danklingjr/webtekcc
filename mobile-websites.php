92b018797933d4580c12837a297e56de38005981
danklingjr <danielklingjr@gmail.com>
2014-10-27 17:24:41 -0400

    Office work

--- mobile-websites.php
+++ mobile-websites.php
@@ -19,7 +19,7 @@ include('includes/header.php') // Include header file //
 
         <?php include('includes/web-nav.php'); ?>
 
-        <div class="content__graphic group">
+        <div class="content__graphic--mobile group">
             
         </div>
 
@@ -31,10 +31,9 @@ include('includes/header.php') // Include header file //
 
             <div class="service__single--wrap grid-full group">
                 <div class="service__single--main grid-4">
-                    <p>Mobile phones have exploded in popularity and have become the new standard in telecommunications. As technology continues to evolve, most mobile phones being sold today are smartphones with built in website browsing.</p>
-                    <p>What that means for website owners is, there is a whole new growing audience of people accessing your website from a small mobile phone. Do you even know how your website looks on a smartphone? If not, you should find out and start developing a mobile strategy for your viewing audience.</p>
-                    <p>Depending on what type of business you are in and why your visitors are accessing your site will help determine what kind of mobile strategy you should be implementing. For instance, if you are a retail business, you might be getting a lot of visitors to your site just to find out your hours, address or phone number. If they are seeing your full desktop site with tons of links and the information they really need quick access to can't be found, that mobile user experience can be frustrating.</p>
-                    <p>There are basically 4 options for mobile website development:</p>
+                    <p>Sometimes the best experience is the simplest one.  Dedicated mobile websites are still a viable option to promote your products or services, as long as they’re designed with content parity in mind.  Content parity is the idea that you serve the same content on your dedicated mobile website as you serve on your desktop version.  Some websites are very large and it can seem like a daunting task to add all of the same content  from the desktop version, so this may be an appropriate use case to leave out some content.  If you do it this way you run the risk of confusing or frustrating users who interact with your site on mobile and desktop.</p>
+                    <p></p>
+                    
                     <ul>
                         <li><b><a href="">Responsive Web Design</a></b> - Build your site to automatically respond to smaller screen sizes</li>
                         <li><b>Separate Mobile Site</b> - Build a totally separate condensed version of your main website</li>
