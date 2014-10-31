25d0c137c5e5608e62123d24951c95b5fd2a5403
danklingjr <danielklingjr@gmail.com>
2014-10-31 12:44:14 -0400

    Overviews

---
+++ internet-marketing.php
@@ -0,0 +1,140 @@
+<?php
+// Define variables for SEO
+$parent = '';
+$parentId = '';
+$pageId = 'web-design';
+$pageTitle = 'Web Design'; // Title of page //
+$pageDescription = 'put page description here'; // Title of page //
+$pageCanonical = 'http://www.url.com/';
+// If we don't want the search engines to see our website just yet
+//$pageRobots = 'noindex,nofollow';
+include('includes/header.php') // Include header file //
+?>
+
+        <section class="hero__page--overview group">
+            <figure class="overview__graphic">
+                <img src="" alt="">
+            </figure>
+            <h1>Internet Marketing</h1>
+            <div class="content__overview--points group">
+                <div class="point--marketing grid-25">
+                    <h3>Custom-Tailored.</h3>
+                    <p>Design shouldn’t just look good, it should function. Our designs are custom so that they fit seemlessly with your brand and business goals yet stand out from the crowd.</p>
+                </div>
+                <div class="point--marketing grid-25">
+                    <h3>Adaptive.</h3>
+                    <p>Our websites are designed to work on any device.  Whether its a smartphone or a desktop your content will adjust to enhance readability and accessibility.</p>
+                </div>
+                <div class="point--marketing grid-25">
+                    <h3>Goal-Driven.</h3>
+                    <p>A user interface is like a joke, if you have to explain it, it isn’t that good. Users shouldn’t be confused or frustrated trying to find products or services on your website.</p>
+                </div>
+                <div class="point--marketing grid-25">
+                    <h3>Flexible.</h3>
+                    <p>No one should be left behind. Web accessibility means people with disabilities can use your website. We develop websites that work for everyone.</p>
+                </div>
+            </div>            
+        </section>
+
+
+        <section class="content__overview--design group">
+            <div class="overview__marketing--description grid-half no-margin">
+                <h2>We don’t just increase traffic,<br>we increase conversions...</h2>
+                <p>We focus on driving high-quality traffic to your website and increasing your company’s or brand’s awareness across the web. When your business is successful, we’re successful—it’s that simple.</p>
+            </div>
+            <div class="overview__marketing--graphic grid-half no-margin">
+                <img src="img/wireframe.png" alt="">
+            </div>
+        </section>
+
+        <section class="content__overview--process group">
+            <h2>...through research and refinement.</h2>
+            <div class="group">
+                <div class="step-1--marketing grid-2">
+                    <h3>Discuss</h3>
+                    <p>We understand that your business goals are unique and we believe that your internet marketing strategy should be unique as well. At WebTek, there is no cookie-cutter, one-size fits all solution.</p>
+                </div>
+                <div class="step-2--marketing grid-2">
+                    <h3>Research</h3>
+                    <p>Research and information gathering is our specialty. First, we’ll take a look at your website to discover your strengths and identify areas that need improvement. Next, we’ll analyze your competitors and conduct industry research.</p>
+                </div>
+                <div class="step-3--marketing grid-2">
+                    <h3>Strategy</h3>
+                    <p>Once we’ve gathered the proper information and identified your business goals and objectives, we’ll detemine the best plan of action for achieving these goals.</p>
+                </div>
+            </div>
+            <div class="group">
+                <div class="step-4--marketing grid-2">
+                    <h3>Implement</h3>
+                    <p>We will execute our internet marketing strategy by using on-page and off-page SEO tactics. We will go right to the source and apply the proper SEO techniques to your website, and we will also claim listings and increase your brand awareness across the web.</p>
+                </div>
+                <div class="step-5--marketing grid-2">
+                    <h3>Analyze</h3>
+                    <p>This is, perhaps, the most important part of the process. At this stage, we analyze the progress over time and determine if your business objectives are being met. Is there room for improvement? What can we do to increase our desired results?</p>
+                </div>
+                <div class="step-6--marketing grid-2">
+                    <h3>Refine</h3>
+                    <p>This is where we make tweaks, expand, or modify your current internet marketing campaign. We implement the necessary changes that we determine in Step 5, then go back to Step 1 and repeat!</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="content__overview--responsive group">
+            <div class="overview__responsive--description group">
+                <h2>Search Engine Optimization is ever-changing...</h2>
+                <p>Google’s search algorithm is made up of over 200 different ranking factors, and this changes approximately _______ times per year. Our internet marketing team is here to help keep you informed of the current industry trends and what we’re doing to help your business stay ahead of the curve.</p>
+                <a href="" class="button">What is responsive design?</a>
+            </div>
+            <div class="overview__responsive--graphic group">
+                <div class="grid-half no-margin">
+                    <img src="" alt="">
+                </div>
+                <div class="grid-half no-margin">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </section>
+
+        <section class="content__overview--development group">
+            <h2>Integrated Marketing</h2>
+            <p>We offer a variety of internet marketing services to fit each business’ unique needs. Depending on your target market and your specific business goals, you may benefit from one or a variety of the services listed below—that’s where we come in. We’re not here to sell you on something, we’re here to provide  you with the proper information so that you can make the best choices possible for the future of your company. We understand that internet marketing can be confusing and overwhelming; that’s why we’re here to answer all of your questions...even some of the ones you haven’t thought of yet.</p>
+            <div class="overview__services group">
+                <div class="service__box--ecommerce bounce grid-2">
+                    <a href="">
+                        <h3>Local Internet Marketing</h3>
+                        <hr>
+                        <p>Learn More</p>
+                    </a>
+                </div>
+                <div class="service__box--cms grid-2">
+                    <a href="">
+                        <h3>Search Engine Optimization</h3>
+                        <hr>
+                        <p>Learn More</p>
+                    </a>
+                </div>
+                <div class="service__box--wordpress grid-2">
+                    <a href="">
+                        <h3>Pay-Per-Click Advertising</h3>
+                        <hr>
+                        <p>Learn More</p>
+                    </a>
+                </div>
+            </div>
+        </section>
+
+        <!-- <section class="content__overview--additional group">
+            <div class="additional-service grid-half">
+                <h4>Web Hosting</h4>
+                <p>WebTek offers commercial grade hosting on dedicated servers that are safely located in a state-of-the-art server facility.</p>
+                <a href="" class="button">Learn More</a>
+            </div>
+            <div class="additional-service grid-half">
+                <h4>Domain Names</h4>
+                <p>WebTek offers commercial grade hosting on dedicated servers that are safely located in a state-of-the-art server facility.</p>
+                <a href="" class="button">Learn More</a>
+            </div>
+        </section> -->
+
+
+<?php include('includes/footer.php'); ?>
---
+++ web-design.php
@@ -0,0 +1,140 @@
+<?php
+// Define variables for SEO
+$parent = '';
+$parentId = '';
+$pageId = 'web-design';
+$pageTitle = 'Web Design'; // Title of page //
+$pageDescription = 'put page description here'; // Title of page //
+$pageCanonical = 'http://www.url.com/';
+// If we don't want the search engines to see our website just yet
+//$pageRobots = 'noindex,nofollow';
+include('includes/header.php') // Include header file //
+?>
+
+        <section class="hero__page--overview group">
+            <figure class="overview__graphic">
+                <img src="img/web-design-graphic.png" alt="">
+            </figure>
+            <h1>Web Design</h1>
+            <div class="content__overview--points group">
+                <div class="point grid-25">
+                    <h3>Custom.</h3>
+                    <p>Design shouldn’t just look good, it should function. Our designs are custom so that they fit seemlessly with your brand and business goals yet stand out from the crowd.</p>
+                </div>
+                <div class="point grid-25">
+                    <h3>Responsive.</h3>
+                    <p>Our websites are designed to work on any device.  Whether its a smartphone or a desktop your content will adjust to enhance readability and accessibility.</p>
+                </div>
+                <div class="point grid-25">
+                    <h3>Usable.</h3>
+                    <p>A user interface is like a joke, if you have to explain it, it isn’t that good. Users shouldn’t be confused or frustrated trying to find products or services on your website.</p>
+                </div>
+                <div class="point grid-25">
+                    <h3>Accessible.</h3>
+                    <p>No one should be left behind. Web accessibility means people with disabilities can use your website. We develop websites that work for everyone.</p>
+                </div>
+            </div>            
+        </section>
+
+
+        <section class="content__overview--design group">
+            <div class="overview__design--description grid-half no-margin">
+                <h2>Great design<br>starts with a plan...</h2>
+                <p>Design shouldn’t just look good, it should function. Our designs are custom so that they fit seemlessly with your brand and business goals yet stand out from the crowd.</p>
+            </div>
+            <div class="overview__design--graphic grid-half no-margin">
+                <img src="img/wireframe.png" alt="">
+            </div>
+        </section>
+
+        <section class="content__overview--process group">
+            <h2>...and every plan is part of a process</h2>
+            <div class="group">
+                <div class="step-1 grid-2">
+                    <h3>Research</h3>
+                    <p>Design shouldn’t just look good, it should function. Our designs are custom so that they fit seemlessly with your brand and business goals yet stand out from the crowd.</p>
+                </div>
+                <div class="step-2 grid-2">
+                    <h3>Concept</h3>
+                    <p>Design shouldn’t just look good, it should function. Our designs are custom so that they fit seemlessly with your brand and business goals yet stand out from the crowd.</p>
+                </div>
+                <div class="step-3 grid-2">
+                    <h3>Design</h3>
+                    <p>Design shouldn’t just look good, it should function. Our designs are custom so that they fit seemlessly with your brand and business goals yet stand out from the crowd.</p>
+                </div>
+            </div>
+            <div class="group">
+                <div class="step-4 grid-2">
+                    <h3>Development</h3>
+                    <p>Design shouldn’t just look good, it should function. Our designs are custom so that they fit seemlessly with your brand and business goals yet stand out from the crowd.</p>
+                </div>
+                <div class="step-5 grid-2">
+                    <h3>Quality Control</h3>
+                    <p>Design shouldn’t just look good, it should function. Our designs are custom so that they fit seemlessly with your brand and business goals yet stand out from the crowd.</p>
+                </div>
+                <div class="step-6 grid-2">
+                    <h3>Go Live</h3>
+                    <p>Design shouldn’t just look good, it should function. Our designs are custom so that they fit seemlessly with your brand and business goals yet stand out from the crowd.</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="content__overview--responsive group">
+            <div class="overview__responsive--description group">
+                <h2>Design done right is responsive.</h2>
+                <p>Responsive web design is the practice of designing and developing one website that adjusts to whatever device you’re using.  All content will adjust to fit the current device size but will maintain optimal readability and usability.</p>
+                <a href="" class="button">What is responsive design?</a>
+            </div>
+            <div class="overview__responsive--graphic group">
+                <div class="grid-half no-margin">
+                    <img src="img/responsive-graphic2.png" alt="">
+                </div>
+                <div class="grid-half no-margin">
+                    <img src="img/responsive-graphic1.png" alt="">
+                </div>
+            </div>
+        </section>
+
+        <section class="content__overview--development group">
+            <h2>&lt;<span>h2</span>&gt;Development&lt;/<span>h2</span>&gt;</h2>
+            <p>&lt;<span>p</span>&gt;Custom web development is usually needed when you want your website to be more interactive or keep track of things. Web Development usually requires databases. Databases are used to help record and organize products, items or records of information. Some examples of custom development using databases are to show inventory, upcoming events or organize products. WebTek uses the main stream platform of PHP for back-end coding and mySQL for database solutions. This is what the majority of custom web development is built in across the web.&lt;/<span>p</span>&gt;</p>
+            <div class="overview__services group">
+                <div class="service__box--ecommerce bounce grid-2">
+                    <a href="">
+                        <h3>Ecommerce Development</h3>
+                        <hr>
+                        <p>Learn More</p>
+                    </a>
+                </div>
+                <div class="service__box--cms grid-2">
+                    <a href="">
+                        <h3>Content Management Systems (CMS)</h3>
+                        <hr>
+                        <p>Learn More</p>
+                    </a>
+                </div>
+                <div class="service__box--wordpress grid-2">
+                    <a href="">
+                        <h3>Wordpress Development</h3>
+                        <hr>
+                        <p>Learn More</p>
+                    </a>
+                </div>
+            </div>
+        </section>
+
+        <section class="content__overview--additional group">
+            <div class="additional-service grid-half">
+                <h4>Web Hosting</h4>
+                <p>WebTek offers commercial grade hosting on dedicated servers that are safely located in a state-of-the-art server facility.</p>
+                <a href="" class="button">Learn More</a>
+            </div>
+            <div class="additional-service grid-half">
+                <h4>Domain Names</h4>
+                <p>WebTek offers commercial grade hosting on dedicated servers that are safely located in a state-of-the-art server facility.</p>
+                <a href="" class="button">Learn More</a>
+            </div>
+        </section>
+
+
+<?php include('includes/footer.php'); ?>
