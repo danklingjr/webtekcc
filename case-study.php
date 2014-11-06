b97d3cdcf28143244e677eee2f25b1d7f0c73f67
danklingjr <danielklingjr@gmail.com>
2014-11-06 16:48:47 -0500

    Case study

--- case-study.php
+++ case-study.php
@@ -11,22 +11,90 @@ $pageCanonical = 'http://www.url.com/';
 include('includes/header.php') // Include header file //
 ?>
 
-        <section class="hero__page--case-study group">
-            <h1>SEO Pricing</h1>
-            <p class="page__description">Search engine marketing done right, takes time and does involve a small investment and commitment on your end.</p>
-            
+        <section class="hero__page--vynecrest group">
+            <img src="img/vynecrest-device-graphic.png" alt="Vynecrest Winery, Devices">
         </section>
 
-        <div class="content__service group">
-            <!-- <div class="service__single--graphic">
-                
-            </div> -->
-
-            <div class="service__single--wrap grid-full group">
-                <div class="service__single--main grid-4">
-                    
+        <div class="content__case-study group">
+            <div class="case-study__header">
+                <h1>Vynecrest Winery</h1>
+                <div class="case-study__tags">
+                    <a href="" class="button__tag">Responsive Design</a><a href="" class="button__tag">ECommerce</a><a href="" class="button__tag">CMS</a>
                 </div>
-                <?php include('includes/testimonial-sidebar.php'); ?>
+            </div>
+
+            <div class="group">
+                <div class="case-study__problem grid-half">
+                    <h3>Problem</h3>
+                    <p>A winery’s image is very important.  Vynecrest realized they had a website that didn’t match their company’s image and brand. Their website was designed for desktop only, which created a problem for customers using the site on mobile devices.</p>
+                </div>
+                <div class="case-study__problem grid-half">
+                    <h3>Solution</h3>
+                    <p>We came up with a responsive solution with a darker, more refined color palette with the use of some textures that made the design seem more classic and established.  The approach is device agnostic meaning it should work at any resolution and on any device.</p>
+                </div>
+            </div>
+        </div>
+
+        <section class="case-study__style-guide">
+            <h2>Style Guide</h2>
+            <p>We created a styleguide that blended the classic wine culture as well as Vynecrest’s beginnings in 1974.</p>
+            <div class="color-palette group">
+                <div class="color-swatch">
+                    <div class="color-swatch__top--green">
+                        
+                    </div>
+                    <div class="color-swatch__bottom">
+                        <span>#798a56</span>
+                    </div>
+                </div><!--
+                --><div class="color-swatch">
+                    <div class="color-swatch__top--red">
+                        
+                    </div>
+                    <div class="color-swatch__bottom">
+                        <span>#600f10</span>
+                    </div>
+                </div><!--
+                --><div class="color-swatch">
+                    <div class="color-swatch__top--orange">
+                        
+                    </div>
+                    <div class="color-swatch__bottom">
+                        <span>#8e441f</span>
+                    </div>
+                </div><!--
+                --><div class="color-swatch">
+                    <div class="color-swatch__top--blue">
+                        
+                    </div>
+                    <div class="color-swatch__bottom">
+                        <span>#517991</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="case-study__breakdown group">
+            <b class="quote"><img src="img/quote.jpg" alt=""></b>
+            <p class="highlight">Our wines speak to our dedication in producing fine wines. With over twenty wines ranging from red to white, dry to sweet, we are committed to making only quality wines. We enjoy what we do and we hope you enjoy the fruits of our labor!</p>
+            <p>This quote says a lot about Vynecrest and their dedication to quality.  We wanted to represent that from the get go on the homepage and follow through</p>
+            <div class="group">
+                <div class="breakdown__graphic">
+                    <div class="browser--top group">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <img src="img/vynecrest-homepage.png" alt="">
+                </div><!--
+                --><div class="breakdown__points">
+                    <b>It all starts at home</b>
+                    <p>Redesigned homepage featuring some really cool stuff that people might like.</p>
+                    <b>It all starts at home</b>
+                    <p>Redesigned homepage featuring some really cool stuff that people might like.</p>
+                    <b>It all starts at home</b>
+                    <p>Redesigned homepage featuring some really cool stuff that people might like.</p>
+                </div>
             </div>
         </div>
 
