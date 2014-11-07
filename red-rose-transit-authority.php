63a857ccd87fff1943ec0e274e845767c5b4a36b
danklingjr <danielklingjr@gmail.com>
2014-11-07 16:58:29 -0500

    Vynecrest and RRTA case studies

---
+++ red-rose-transit-authority.php
@@ -0,0 +1,144 @@
+<?php
+// Define variables for SEO
+$parent = 'Portfolio';
+$parentId = 'portfolio';
+$pageId = 'red-rose-transit-authority';
+$pageTitle = 'Red Rose Transit Authority Web Design'; // Title of page //
+$pageDescription = 'put page description here'; // Title of page //
+$pageCanonical = 'http://www.url.com/';
+// If we don't want the search engines to see our website just yet
+//$pageRobots = 'noindex,nofollow';
+include('includes/header.php') // Include header file //
+?>
+
+        <section class="hero__page--rrta group">
+            <img src="img/rrta-device-graphic.png" alt="Red Rose Transit, Devices">
+        </section>
+
+        <div class="content__case-study group">
+            <div class="case-study__header">
+                <h1>Red Rose Transit Authority</h1>
+                <div class="case-study__tags">
+                    <a href="" class="button__tag">Responsive Design</a><a href="" class="button__tag">Wordpress</a><a href="" class="button__tag">CMS</a><a href="http://www.vynecrest.com" class="button" target="_blank">View Site</a>
+                </div>
+            </div>
+
+            <div class="group">
+                <div class="case-study__problem grid-half">
+                    <h3>Problem</h3>
+                    <p>Public transportation authorities have an obligation to serve everyone, regardless if they're disabled or not.  The Red Rose Transit Authority had a website that wasn't accessible to people with disabilities.  Their website wasn't optimized for mobile and their site architecture had hidden navigation that wasn't visible until you visited a service page.  Performance was also not optimized for mobile meaning it loaded slower over weak cellular reception.</p>
+                </div>
+                <div class="case-study__problem grid-half">
+                    <h3>Solution</h3>
+                    <p>We designed and developed a responsive solution with a strong focus on accessibility and section 508 compliance.  We organized the navigation to make it easier to navigate to every section of the website.  We placed a strong emphasis on route schedules, route alerts, and route detours to make it easier for the general public to stay up to date.  The website was optimized to load over weak cellular reception, which greatly increases the usability of the site.</p>
+                </div>
+            </div>
+        </div>
+
+        <section class="case-study__style-guide">
+            <h2>Style Guide</h2>
+            <p>We created a styleguide that added more contrast and is vibrant. These colors helps to make the site bright and inviting.</p>
+            <div class="color-palette group">
+                <div class="color-swatch">
+                    <div class="color-swatch__top--rrta1">
+                        
+                    </div>
+                    <div class="color-swatch__bottom">
+                        <span>#d92e3a</span>
+                    </div>
+                </div><!--
+                --><div class="color-swatch">
+                    <div class="color-swatch__top--rrta2">
+                        
+                    </div>
+                    <div class="color-swatch__bottom">
+                        <span>#3a6cc5</span>
+                    </div>
+                </div><!--
+                --><div class="color-swatch">
+                    <div class="color-swatch__top--rrta3">
+                        
+                    </div>
+                    <div class="color-swatch__bottom">
+                        <span>#f8bb00</span>
+                    </div>
+                </div><!--
+                --><div class="color-swatch">
+                    <div class="color-swatch__top--rrta4">
+                        
+                    </div>
+                    <div class="color-swatch__bottom">
+                        <span>#252525</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="case-study__breakdown group">
+            <b class="quote"><img src="img/quote.jpg" alt=""></b>
+            <p class="highlight">The Red Rose Transit Authority exists to provide effective public transportation services to the citizens of Lancaster County and to perform these services at the highest standards of safety, courtesy, reliability, and efficiency.</p>
+            <p>The Red Rose Transit Authority has very high standards so the website needed to match those standards.  The website had to be just as reliable as their public transportation service and make those services more efficient through its use.</p>
+
+            <div class="breakdown group">
+                <div class="breakdown__graphic--rrta">
+                    <img src="img/rrta-devices.png" alt="">
+                    <span class="point-6--rrta">1</span>
+                    <span class="point-7--rrta">2</span>
+                    <span class="point-8--rrta">3</span>
+                </div><!--
+                --><div class="breakdown__points">
+                    <div class="breakdown__point">
+                        <b><span>1</span> Reorganized for Mobile</b>
+                        <p>Presenting the brand front and center was important to show current and new customers that they are an established, respected winery.</p>
+                    </div>
+                    <div class="breakdown__point">
+                        <b><span>2</span> Responsive Transit Schedules</b>
+                        <p>For a winery, focusing on their wine is extremely important. This section has a call to action to view their wine as well as order it online.</p>
+                    </div>
+                    <div class="breakdown__point">
+                        <b><span>3</span> Access To Route Delays</b>
+                        <p>For a winery, focusing on their wine is extremely important. This section has a call to action to view their wine as well as order it online.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="breakdown group">
+                <div class="breakdown__graphic--rrta">
+                    <img src="img/rrta-routes.jpg" alt="">
+                    <span class="point-1--rrta">1</span>
+                    <span class="point-2--rrta">2</span>
+                </div><!--
+                --><div class="breakdown__points">
+                    <div class="breakdown__point">
+                        <b><span>1</span> Focus On Schedules</b>
+                        <p>Presenting the brand front and center was important to show current and new customers that they are an established, respected winery.</p>
+                    </div>
+                    <div class="breakdown__point">
+                        <b><span>2</span> Access to schedule info</b>
+                        <p>For a winery, focusing on their wine is extremely important. This section has a call to action to view their wine as well as order it online.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="breakdown group">
+                <div class="breakdown__graphic--rrta">
+                    <img src="img/rrta-footer.jpg" alt="">
+                    <span class="point-3--rrta">1</span>
+                    <span class="point-5--rrta">2</span>
+                </div><!--
+                --><div class="breakdown__points">
+                    <div class="breakdown__point">
+                        <b><span>1</span> Showing All Services</b>
+                        <p>Presenting the brand front and center was important to show current and new customers that they are an established, respected winery.</p>
+                    </div>
+                    <div class="breakdown__point">
+                        <b><span>2</span> Showing Secondary Links</b>
+                        <p>Presenting the brand front and center was important to show current and new customers that they are an established, respected winery.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+<?php include('includes/footer.php'); ?>
--- vynecrest-winery.php
+++ vynecrest-winery.php
@@ -1,9 +1,9 @@
 <?php
 // Define variables for SEO
-$parent = 'SEO';
-$parentId = 'seo';
-$pageId = 'seo-pricing';
-$pageTitle = 'SEO Pricing'; // Title of page //
+$parent = 'Portfolio';
+$parentId = 'portfolio';
+$pageId = 'vynecrest-winery';
+$pageTitle = 'Vynecrest Winery Web Design'; // Title of page //
 $pageDescription = 'put page description here'; // Title of page //
 $pageCanonical = 'http://www.url.com/';
 // If we don't want the search engines to see our website just yet
@@ -19,7 +19,7 @@ include('includes/header.php') // Include header file //
             <div class="case-study__header">
                 <h1>Vynecrest Winery</h1>
                 <div class="case-study__tags">
-                    <a href="" class="button__tag">Responsive Design</a><a href="" class="button__tag">ECommerce</a><a href="" class="button__tag">CMS</a>
+                    <a href="" class="button__tag">Responsive Design</a><a href="" class="button__tag">ECommerce</a><a href="" class="button__tag">CMS</a><a href="http://www.vynecrest.com" class="button" target="_blank">View Site</a>
                 </div>
             </div>
 
@@ -37,7 +37,7 @@ include('includes/header.php') // Include header file //
 
         <section class="case-study__style-guide">
             <h2>Style Guide</h2>
-            <p>We created a styleguide that blended the classic wine culture as well as Vynecrest’s beginnings in 1974.</p>
+            <p>We created a styleguide that blended the classic wine culture as well as Vynecrest’s modern beginnings in 1974.</p>
             <div class="color-palette group">
                 <div class="color-swatch">
                     <div class="color-swatch__top--green">
@@ -77,8 +77,9 @@ include('includes/header.php') // Include header file //
         <div class="case-study__breakdown group">
             <b class="quote"><img src="img/quote.jpg" alt=""></b>
             <p class="highlight">Our wines speak to our dedication in producing fine wines. With over twenty wines ranging from red to white, dry to sweet, we are committed to making only quality wines. We enjoy what we do and we hope you enjoy the fruits of our labor!</p>
-            <p>This quote says a lot about Vynecrest and their dedication to quality.  We wanted to represent that from the get go on the homepage and follow through</p>
-            <div class="group">
+            <p>We designed the Vynecrest homepage to focus on and compliment Vynecrest's dedication to producing quaility wines. Their dedication really impressed us so we wanted to match that level of quality and dedication with this design.</p>
+
+            <div class="breakdown group">
                 <div class="breakdown__graphic">
                     <div class="browser--top group">
                         <span></span>
@@ -86,14 +87,33 @@ include('includes/header.php') // Include header file //
                         <span></span>
                     </div>
                     <img src="img/vynecrest-homepage.png" alt="">
+                    <span class="point-1">1</span>
+                    <span class="point-2">2</span>
+                    <span class="point-3">3</span>
+                    <span class="point-4">4</span>
+                    <span class="point-5">5</span>
                 </div><!--
                 --><div class="breakdown__points">
-                    <b>It all starts at home</b>
-                    <p>Redesigned homepage featuring some really cool stuff that people might like.</p>
-                    <b>It all starts at home</b>
-                    <p>Redesigned homepage featuring some really cool stuff that people might like.</p>
-                    <b>It all starts at home</b>
-                    <p>Redesigned homepage featuring some really cool stuff that people might like.</p>
+                    <div class="breakdown__point">
+                        <b><span>1</span> Present the brand</b>
+                        <p>Presenting the brand front and center was important to show current and new customers that they are an established, respected winery.</p>
+                    </div>
+                    <div class="breakdown__point">
+                        <b><span>2</span> Focus on the wine</b>
+                        <p>For a winery, focusing on their wine is extremely important. This section has a call to action to view their wine as well as order it online.</p>
+                    </div>
+                    <div class="breakdown__point">
+                        <b><span>3</span> Highlight locations</b>
+                        <p>They run various events at the winery so we wanted to highlight those as well as their multiple locations where customers can visit.</p>
+                    </div>
+                    <div class="breakdown__point">
+                        <b><span>4</span> Wine of the month</b>
+                        <p>They needed a place to market their monthly deals that stood out and didn't clutter the layout.</p>
+                    </div>
+                    <div class="breakdown__point">
+                        <b><span>5</span> Large Footer</b>
+                        <p>We designed a large footer to make their wines and contact info accessible on any page.</p>
+                    </div>
                 </div>
             </div>
         </div>
